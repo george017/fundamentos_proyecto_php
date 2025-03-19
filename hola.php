77e375110f3f701eb3463ed8b34c778d4d3c278a
Jorge Aguilar <jorgea@galileo.edu>
2025-03-19 16:46:17 -0600

    firt commit

---
+++ hola.php
@@ -0,0 +1,6 @@
+<?php
+$nombre = "Jorge Luis"; 
+$apellido = "Aguilar Perez";
+echo "Hola Mundo, " . $nombre . " " . $apellido; 
+echo '<br><img src="/Actividad3/proyecto_php/gatoLeyendo.jpeg" width="300" height="200">';
+?>
